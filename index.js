742238cbeaa1d56cca000d0a807bbe9eeabd6ee3
Amin Latifi <a.latifi.al@gmail.com>
2021-08-16 16:38:03 +0430

    Initialize

---
+++ index.js
@@ -0,0 +1,17 @@
+const {
+  FailClosedVault,
+  ForeignGivethBridge,
+  GivethBridge,
+  IForeignGivethBridge,
+  Pausable,
+  Vault,
+} = require('./lib/contracts');
+
+module.exports = {
+  FailClosedVault,
+  ForeignGivethBridge,
+  GivethBridge,
+  IForeignGivethBridge,
+  Pausable,
+  Vault,
+};
---
+++ src/contracts.js
@@ -0,0 +1,24 @@
+const generateClass = require('@giveth/eth-contract-class').default;
+
+const contracts = {};
+
+[
+  'FailClosedVault.json',
+  'ForeignGivethBridge.json',
+  'GivethBridge.json',
+  'IForeignGivethBridge.json',
+  'Passable.json',
+  'Vault.json',
+].forEach(file => {
+  // eslint-disable-next-line global-require,import/no-dynamic-require
+  const { contractName, compilerOutput } = require(`./${file}`);
+
+  if (compilerOutput.abi && compilerOutput.evm.bytecode.object.length > 0) {
+    contracts[contractName] = generateClass(
+      compilerOutput.abi,
+      `0x${compilerOutput.evm.bytecode.object}`,
+    );
+  }
+});
+
+module.exports = contracts;
